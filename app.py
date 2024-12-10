5b561a3ea6c313da9e734f7b1ec7c037693eda7b
Jimmy p <boldnbubbles@gmail.com>
2024-12-10 07:47:54 -0900

    Added new html files and updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from routes import *
+@app.route('/')
+def home():
+    return "Welcome to the Time Hour Sheet App!"
 
 if __name__ == '__main__':
     app.run(debug=True)
